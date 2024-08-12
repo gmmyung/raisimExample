56944a5e7cf6ef244b8926ae1c465e3bb2c7a724
Gyungmin Myung <gmmyung@kaist.ac.kr>
2024-08-12 10:45:25 +0900

    reproduced.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,59 @@
-#include "raisim/World.hpp"
 #include "raisim/RaisimServer.hpp"
+#include "raisim/World.hpp"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
 
-int main(int argc, char* argv[]) {
-  auto binaryPath = raisim::Path::setFromArgv(argv[0]);
-
-  raisim::World world;
-  auto laikago = world.addArticulatedSystem(binaryPath.getDirectory() + "\\rsc\\laikago\\laikago.urdf");
-  auto ball = world.addSphere(0.1, 1);
-  auto ground = world.addGround(-2);
-  world.setTimeStep(0.002);
-
-  /// launch raisim server for visualization. Can be visualized using raisimUnity
-  raisim::RaisimServer server(&world);
-  server.launchServer();
-
-  for (int i=0; i<10000; i++) {
-    RS_TIMED_LOOP(world.getTimeStep()*1e6);
-    server.integrateWorldThreadSafe();
+void readImage(const char *filename, raisim::DepthCamera *depthSensor) {
+  auto im = depthSensor->getDepthArray();
+  std::vector<uint8_t> im_u8(im.size());
+  for (int i = 0; i < im.size(); i++) {
+    float depth = im[i];
+    depth *= 10;
+    if (std::isnan(depth))
+      depth = 255.0;
+    im_u8[i] = (uint8_t)depth;
   }
+  stbi_write_png(filename, 64, 64, 1, im_u8.data(), 64);
+}
 
-  server.killServer();
+int main(int argc, char *argv[]) {
+  raisim::World world;
+  auto binaryPath = raisim::Path::setFromArgv(argv[0]);
+  uint nGrid = 128;
+  std::vector<double> heightVec(nGrid * nGrid, -1.0);
+  world.addHeightMap(nGrid, nGrid, 10.0, 10.0, 0.0, 0.0, heightVec);
+  // world.addGround(-2);
+  world.setTimeStep(0.002);
+  auto robot = world.addArticulatedSystem(
+      binaryPath.getDirectory() + "/rsc/anymal/urdf/anymal_depth_sensor.urdf");
+
+  Eigen::VectorXd gc;
+  gc.setZero(robot->getGeneralizedCoordinateDim());
+  gc << 0, 0, 0.50, 1.0, 0.0, 0.0, 0.0, 0.03, 0.4, -0.8, -0.03, 0.4, -0.8, 0.03,
+      -0.4, 0.8, -0.03, -0.4, 0.8;
+  robot->setGeneralizedCoordinate(gc);
+
+  auto depthSensor = robot->getSensorSet("depth_camera")
+                         ->getSensor<raisim::DepthCamera>("depth");
+  depthSensor->setMeasurementSource(raisim::Sensor::MeasurementSource::RAISIM);
+  depthSensor->update(world);
+
+  std::string fileName = binaryPath.getDirectory().getString() + "/depth_x.png";
+  readImage(fileName.c_str(), depthSensor);
+
+  for (int i = 0; i < 20; i++) {
+    // rotate robot
+    raisim::Mat<3, 3> rot;
+    raisim::angleAxisToRotMat({0, 0, 1}, 0.2 * i, rot);
+    raisim::Vec<4> quat;
+    raisim::rotMatToQuat(rot, quat);
+    gc.segment(3, 4) = quat.e();
+
+    robot->setGeneralizedCoordinate(gc);
+    depthSensor->update(world);
+
+    fileName = binaryPath.getDirectory().getString() + "/depth_rotated_" +
+               std::to_string(i) + ".png";
+    readImage(fileName.c_str(), depthSensor);
+  }
 }
